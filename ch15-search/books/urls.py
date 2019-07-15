d1a52e8c10102b8a39f47a3e343d2f1bcdc775b0
William Vincent <will@wsvincent.com>
2019-07-15 12:23:13 -0400

    ch15 update

---
+++ ch15-search/books/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from .views import BookListView, BookDetailView, SearchResultsView
+
+urlpatterns = [
+    path('', BookListView.as_view(), name='book_list'),
+    path('<uuid:pk>', BookDetailView.as_view(), name='book_detail'),
+    path('search/', SearchResultsView.as_view(), name='search_results'),
+]
---
+++ ch15-search/bookstore_project/urls.py
@@ -0,0 +1,17 @@
+from django.conf import settings 
+from django.conf.urls.static import static 
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    # Django admin
+    path('admin/', admin.site.urls),
+
+    # User management
+    path('accounts/', include('allauth.urls')),
+
+    # Local apps
+    path('', include('pages.urls')),
+    path('books/', include('books.urls')),
+    path('orders/', include('orders.urls')), 
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
