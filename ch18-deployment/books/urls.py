7611a743ee1d6060757105fc2fd6f10c5981dffd
William Vincent <will@wsvincent.com>
2019-09-10 11:20:51 -0400

    rename SearchResultsView to SearchResultsListView

--- ch18-deployment/books/urls.py
+++ ch18-deployment/books/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import BookListView, BookDetailView, SearchResultsView
+from .views import BookListView, BookDetailView, SearchResultsListView
 
 urlpatterns = [
     path('', BookListView.as_view(), name='book_list'),
     path('<uuid:pk>', BookDetailView.as_view(), name='book_detail'),
-    path('search/', SearchResultsView.as_view(), name='search_results'),
+    path('search/', SearchResultsListView.as_view(), name='search_results'),
 ]
--- ch18-deployment/books/views.py
+++ ch18-deployment/books/views.py
@@ -2,24 +2,24 @@ from django.db.models import Q
 
 from django.contrib.auth.mixins import (
     LoginRequiredMixin,
-    PermissionRequiredMixin 
+    PermissionRequiredMixin
 )
 from django.views.generic import ListView, DetailView
 
 from .models import Book
 
 
-class BookListView(LoginRequiredMixin, ListView): 
+class BookListView(LoginRequiredMixin, ListView):
     model = Book
     context_object_name = 'book_list'
     template_name = 'books/book_list.html'
-    login_url = 'account_login' 
+    login_url = 'account_login'
 
 
 class BookDetailView(
-    LoginRequiredMixin, 
-    PermissionRequiredMixin, 
-    DetailView): 
+    LoginRequiredMixin,
+    PermissionRequiredMixin,
+    DetailView):
     model = Book
     context_object_name = 'book'
     template_name = 'books/book_detail.html'
@@ -27,12 +27,12 @@ class BookDetailView(
     permission_required = 'books.special_status'
 
 
-class SearchResultsView(ListView): 
+class SearchResultsListView(ListView): 
     model = Book
     context_object_name = 'book_list'
     template_name = 'books/search_results.html'
 
-    def get_queryset(self): 
+    def get_queryset(self):
         query = self.request.GET.get('q')
         return Book.objects.filter(
             Q(title__icontains=query) | Q(author__icontains=query)
