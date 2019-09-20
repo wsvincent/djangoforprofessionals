4375c5931ef9b46a04553a057da454b68cf38f27
William Vincent <will@wsvincent.com>
2019-09-20 13:47:04 -0400

    update Meta classes of users/forms.py

--- ch3-books/users/forms.py
+++ ch3-books/users/forms.py
@@ -4,13 +4,13 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 class CustomUserCreationForm(UserCreationForm):
 
-    class Meta(UserCreationForm):
+    class Meta:
         model = get_user_model()
         fields = ('email', 'username',)
 
 
 class CustomUserChangeForm(UserChangeForm):
 
-    class Meta(UserChangeForm):
+    class Meta:
         model = get_user_model()
         fields = ('email', 'username',)
